1c466d017f24c5131f27b8b339c7618211df7f2b
‫abdulrahman Althobaiti <althobiti.433@gmail.com>
2026-08-07 14:58:05 +0300

    Add shared page footer
    Add the shared footer with the club name and copyright information.

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer>
+  <div class="container footer-content">
+    <div>
+      <h3>DataSphere</h3>
+      <p>Data Science &amp; AI Club</p>
+    </div>
+    <p>&copy; 2026 DataSphere Club</p>
+    </div>
+</footer>
+</body>
+</html>
