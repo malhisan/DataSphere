6df09f4cc3519d5fafccef6424aefba63c8b5c61
Balamiri18 <baderalamiri26@gmail.com>
2026-08-10 14:26:59 +0300

    Refactor registration logic and improve error handling

--- register.php
+++ register.php
@@ -115,7 +115,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $$file = fopen($registrationsFile, "a");
 
 if ($file === false) {
-    die("Cannot open file: " . $registrationsFile);
+   $errors[] = "The registration could not be saved.";
 } else {
     flock($file, LOCK_EX);
             if ($fileIsEmpty) {
